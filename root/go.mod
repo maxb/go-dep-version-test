a8ad802962050c9ee2a4a5f7f3c58c69fa8c47f3
Max Bowsher <maxbowsher@gmail.com>
2019-03-10 15:55:47 +0000

    Introduce a dependency on non-module gamma library in this repository.

--- root/go.mod
+++ root/go.mod
@@ -1,6 +1,7 @@
 module github.com/maxb/go-dep-version-test/root
 
 require (
+	github.com/maxb/go-dep-version-test v3.0.0+incompatible
 	github.com/maxb/go-dep-version-test/alpha/v2 v2.0.2
 	github.com/maxb/go-dep-version-test/beta v1.0.0
 )
--- root/main.go
+++ root/main.go
@@ -4,10 +4,12 @@ import (
     "fmt"
     "github.com/maxb/go-dep-version-test/alpha/v2"
     "github.com/maxb/go-dep-version-test/beta"
+    "github.com/maxb/go-dep-version-test/gamma"
 )
 
 func main() {
     fmt.Println("Hello, world!")
     fmt.Println(alpha.Alpha())
     fmt.Println(beta.Beta())
+    fmt.Println(gamma.Gamma())
 }

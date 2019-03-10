23158ac735303973b720f0d3097fd83826b08fbe
Max Bowsher <maxbowsher@gmail.com>
2019-03-10 14:41:10 +0000

    Create root with a dependency on the initial version of alpha.

---
+++ root/go.mod
@@ -0,0 +1,3 @@
+module github.com/maxb/go-dep-version-test/root
+
+require github.com/maxb/go-dep-version-test/alpha v0.0.0-20190310143927-c879e17e3740
---
+++ root/main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+    "fmt"
+    "github.com/maxb/go-dep-version-test/alpha"
+)
+
+func main() {
+    fmt.Println("Hello, world!")
+    fmt.Println(alpha.Alpha())
+}

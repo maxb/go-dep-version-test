b6c34d16b1fe30e9ff7e7de8e2cb17cadc6d5fde
Max Bowsher <maxbowsher@gmail.com>
2019-03-10 14:53:52 +0000

    Update root with dependency on beta.

--- root/go.mod
+++ root/go.mod
@@ -1,3 +1,6 @@
 module github.com/maxb/go-dep-version-test/root
 
-require github.com/maxb/go-dep-version-test/alpha v1.0.0
+require (
+	github.com/maxb/go-dep-version-test/alpha v1.0.0
+	github.com/maxb/go-dep-version-test/beta v1.0.0
+)
--- root/main.go
+++ root/main.go
@@ -3,9 +3,11 @@ package main
 import (
     "fmt"
     "github.com/maxb/go-dep-version-test/alpha"
+    "github.com/maxb/go-dep-version-test/beta"
 )
 
 func main() {
     fmt.Println("Hello, world!")
     fmt.Println(alpha.Alpha())
+    fmt.Println(beta.Beta())
 }
